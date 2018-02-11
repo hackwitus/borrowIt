58333d8a64840a30c49a8832a69a96414276b856
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-02-11 16:01:36 -0500

    set up for merge

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
