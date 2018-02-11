99acbdc0ea9945e8a5e2f40005014f4f05a8693f
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-02-11 00:44:26 -0500

    fixed and working

--- client/src/components/InventoryTable.js
+++ client/src/components/InventoryTable.js
@@ -1,45 +1,48 @@
 import React from 'react';
 
-var width = {
-  width: '100%',
-  textAlign: 'center',
-};
-
-var i = -1;
-
-const renderTable = (items, inventoryTableScope) => {
-  console.log(items)
-  return items ?  (
-    <table style={width}>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">-</th>
-        <th scope="col">Quantity</th>
-        <th scope="col">+</th>
-        <th scope="col">Available</th>
-        <th scope="col">Name</th>
-        <th scope="col">Description</th>
-        <th scope="col">Owner</th>
-      </tr>
-      {items.map(item => (
-        <tr>
-          <th scope="row">{i++}</th>
-          <th scope="row"><button>-</button></th>
-          <th scope="row">{item.quantity}</th>
-          <th scope="row"><button onClick={inventoryTableScope.handleAddItem.bind(this, item)}>+</button></th>
-          <td><span className={item.quantity > 0 ? ("oi oi-check text-success") : ("oi oi-x text-danger")}></span></td>
-          <td>{item.name}</td>
-          <td>{item.description}</td>
-          <td>{item.owner}</td>
-        </tr>
-      ))}
-    </table>
-  ) : (
-    <p>Your table is empty.</p>
-  )
-}
-
 class InventoryTable extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.renderTable = this.renderTable.bind(this)
+  }
+
+  renderTable() {
+    const items = this.props.inventoryItems || []
+    return items.length > 0 ?  (
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">-</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">+</th>
+            <th scope="col">Available</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Owner</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, i) => (
+            <tr key={i}>
+              <th scope="row">{i}</th>
+              <th scope="row"><button> - </button></th>
+              <th scope="row">{item.quantity}</th>
+              <th scope="row"><button onClick={this.handleAddItem.bind(this, item)}>+</button></th>
+              <td><span className={item.quantity > 0 ? ("oi oi-check text-success") : ("oi oi-x text-danger")}></span></td>
+              <td>{item.name}</td>
+              <td>{item.description}</td>
+              <td>{item.owner}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    ) : (
+      <p>Your table is empty.</p>
+    )
+  }
+
   handleAddItem(item, e) {
     this.props.onAddItem(item)
   }
@@ -47,7 +50,7 @@ class InventoryTable extends React.Component {
   render() {
     return (
       <div>
-        { renderTable(this.props.inventoryItems, this) }
+        { this.renderTable() }
       </div>
     )
   }
