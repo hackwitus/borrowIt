ce0e35dbd36359a10ff84d30de72ed43a57735e8
shawn <shawntoubeau@gmail.com>
2018-02-11 00:56:06 -0500

    Started working on cart.js

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
+
+const renderCart = items => {
+  console.log(items)
+  return items ? (
+    <ul>
+    {items.map(item => (
+      <li>item.name</li>
+    ))}
+    </ul>
+  ) : (
+    <p>Your cart is empty.</p>
+  )
+}
+
 class Cart extends React.Component {
   render() {
     return (
       <div>
         <h3>Items in your cart</h3>
-        <ul>
-          <li>Raspberry Pi 3</li>
-          <li>Arduino Uno</li>
-        </ul>
+        { renderCart(this.props.cartItems) }
       </div>
     )
   }
