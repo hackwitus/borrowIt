e56a2579b368b570d0cd05ea5f4335245796f4b5
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-02-11 18:02:41 -0500

    Add functional transaction table

--- client/src/components/TransactionTable.js
+++ client/src/components/TransactionTable.js
@@ -5,6 +5,8 @@ class TransactionTable extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      transactions: [],
+      customer: {},
       items: [
         {
           "name": "Arduino Uno"
@@ -17,8 +19,69 @@ class TransactionTable extends React.Component {
         }
       ]
     }
+    this.getTransactionFromAPI = this.getTransactionFromAPI.bind(this)
+    this.getCustomerFromAPI = this.getCustomerFromAPI.bind(this)
+  }
+  handleEdit(transaction) {
+    console.log(transaction)
+  }
+  handleDelete(transaction) {
+    fetch('https://api-ahtaxdhvbo.now.sh/transactions/delete', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({ transaction })
+    }) 
+      .then(res => res.json())
+      .then(msg => {
+        console.log(msg)
+        this.getTransactionFromAPI()
+      })
+      .catch(err => console.log(err))
+  }
+  handleReturn(transaction) {
+    fetch('https://api-ahtaxdhvbo.now.sh/transactions/returned', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({ transaction })
+    }) 
+      .then(res => res.json())
+      .then(msg => {
+        console.log(msg)
+        this.getTransactionFromAPI()
+      })
+      .catch(err => console.log(err))
+  }
+  getTransactionFromAPI() {
+    fetch('https://api-ahtaxdhvbo.now.sh/transactions') 
+      .then(res => res.json())
+      .then(transactions => {
+        this.setState({ transactions })
+      })
+      .catch(err => console.log(err))
+  }
+  getCustomerFromAPI(phoneNumber) {
+    fetch(`https://api-ahtaxdhvbo.now.sh/customers/${phoneNumber}`) 
+      .then(res => res.json())
+      .then(customer => {
+        this.setState({ customer })
+      })
+      .catch(err => console.log(err))
+  }
+  async getCustomerName(phoneNumber) {
+    const customer = await this.getCustomerFromAPI(phoneNumber)
+    return (
+      <span>{customer.name}</span> 
+    )
+  }
+  componentDidMount() {
+    this.getTransactionFromAPI()
   }
   render() {
+    const transactions = this.state.transactions || []
     return (
       <table className="table">
         <thead>
@@ -26,16 +89,40 @@ class TransactionTable extends React.Component {
             <th>Customer Name</th>
             <th>Items</th>
             <th>Collateral</th>
-            <th>Return</th>
+            <th>Controls</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Ethan Arrowood</td>
-            <td><ItemDropdown items={this.state.items}/></td>
-            <td>Student ID ending in 4367</td>
-            <td><button className="btn btn-success">Return</button></td>
-          </tr>
+          {
+            transactions.map((transaction, i) => (
+              <tr key={i}>
+                <td>{transaction.customer}</td>
+                <td><ItemDropdown items={transaction.items ? transaction.items.split(",") : []} /></td>
+                <td>{transaction.collateral}</td>
+                <td>
+                  <div className="btn-group">
+                    <button 
+                      className="btn btn-success" 
+                      type="button"
+                      onClick={this.handleReturn.bind(this, transaction.id)}
+                      disabled={transaction.timeReturned}
+                    >Return</button>
+                    <button 
+                      className="btn btn-danger" 
+                      type="button"
+                      onClick={this.handleDelete.bind(this, transaction.id)}
+                      disabled={!transaction.timeReturned}
+                    >Delete</button>
+                    <button
+                      className="btn btn-info"
+                      type="button"
+                      onClick={this.handleEdit.bind(this, transaction.id)}
+                    >Edit</button>
+                  </div>
+                </td>
+              </tr>
+            ))
+          }
         </tbody>
       </table>
     )
